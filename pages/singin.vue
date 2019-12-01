93771859c8a1d465d622f582d9b2d023cac0017f
Daniel_Martinez24 <dan24diaz@gmail.com>
2019-12-01 00:59:00 -0600

    añadido el creador de cuenta

--- pages/singin.vue
+++ pages/singin.vue
@@ -1,19 +1,12 @@
 <template>
-  <div id="main">
-    
-    <p>Con <strong>shop & save</strong> puedes llevar un control sobre tus finanzas personales</p>
-    <img
-      id="imgRedonda"
-      src="https://firebasestorage.googleapis.com/v0/b/deshifrut-30515.appspot.com/o/image004.png?alt=media&token=0071b3f4-0f5a-4a71-ad63-2563c435fab5"
-    />
-    <h1 id="title">Registrate !</h1>
-    <br />
-    <br />
+  <div class="container">
+      <p>Con <strong>shop & save</strong> puedes llevar un control sobre tus finanzas personales</p>
+      <img src="https://firebasestorage.googleapis.com/v0/b/shop-and-save.appspot.com/o/chanchos%2FChanchos-asombrado.png?alt=media&token=a449ca67-96dc-438c-adfd-37610effe402"></img>
     <div id="txtEmail">
       <el-input
         id="txtEmail"
         v-model="correo"
-        placeholder="Please input"
+        placeholder="Ingresa tu correo"
         type="email"
         name="txtEmail"
       ></el-input>
@@ -22,16 +15,18 @@
     <div id="txtPassword">
       <el-input
         v-model="contrasenia"
-        placeholder="Please input password"
+        placeholder="Ingresa tu contraseña"
         show-password
       ></el-input>
     </div>
-
-    <el-button id="btnLogin" type="primary">Entrar</el-button>
+     <el-button type="primary" @click="siguiente()" id="singin">Siguiente</el-button>
+     
+    
   </div>
 </template>
 
 <script>
+import { auth } from '@/plugins/firebase';
 
 export default {
   data() {
@@ -39,46 +34,95 @@ export default {
       correo: '',
       contrasenia: ''
     }
+  },
+  methods: {
+    siguiente(){
+      
+      // cambiar esto, a tener aprovación de firebase
+      if (this.contrasenia != '' && this.correo !="") {
+        auth.createUserWithEmailAndPassword(this.correo, this.contrasenia)
+        .then(
+          this.$message({
+          message: 'La cuante fue creada exitosamente.',
+        })
+        )
+        .then(
+          this.$router.push({ path: '/panel' })
+        )
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          this.$message({
+          message: 'error.' + errorCode + ' ' + errorMessage,
+          type: 'error'
+        });
+        });
+      }
+      else {
+       this.$message({
+          message: 'Llena los campos correctamente.',
+          type: 'error'
+        });
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
-#main {
+.container {
   position: fixed;
   width: 100%;
   height: 100%;
-  /*https://encycolorpedia.es/a749bf*/
-  background-color: #b397ba;
-  grid-template-columns: 10% 80% 10%;
-  grid-template-rows: 10% 30% 10% 3% 7% 7% 7% 26%;
   display: grid;
+  grid-template-columns: 10% 80% 10%;
+  grid-template-rows: 33% 30% 10% 10% 10% 7%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
+
+.container img {
+  grid-column-start: 2;
+  grid-row-start: 2;
+  width: 60%;
+  display:block;
+  margin:auto;
+}
+
 #txtEmail {
   grid-column-start: 2;
-  grid-row-start: 5;
+  grid-row-start: 3;
+  width: 100%;
+  display:block;
+  margin:auto;
 }
 #txtPassword {
   grid-column-start: 2;
-  grid-row-start: 6;
+  grid-row-start: 4;
+  
+  width: 100%;
+  display:block;
+  margin:auto;
 }
-#btnLogin {
+#singin {
   grid-column-start: 2;
-  grid-row-start: 7;
+  grid-row-start: 5;
+  
+  width: 80%;
+  display:block;
+  margin:auto;
 }
-#title {
-  text-align: center;
+
+.container p {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 300;
+  font-size: 100%;
+  color: #35495e;
+  letter-spacing: 1px;
   grid-column-start: 2;
-  grid-row-start: 3;
-  color: #ffffff;
-}
-#imgRedonda {
-  display: block;
-  margin: auto;
-  grid-column-start: 2;
-  grid-row-start: 2;
-  width: 200px;
-  height: 200px;
-  border-radius: 150px;
+  grid-row-start: 0;
 }
 </style>
